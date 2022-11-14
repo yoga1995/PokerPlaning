37c06485f2848120e3d6090e3e82f52a5273e4c0
Nikhilareddy1995 <nikhila.mits@gmail.com>
2022-11-14 11:55:28 -0500

    Initial Commit

--- src/main/java/com/pokerplaning/PokerPlaningApplication.java
+++ src/main/java/com/pokerplaning/PokerPlaningApplication.java
@@ -2,7 +2,11 @@ package com.pokerplaning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author Nikhila
+ *
+ */
 @SpringBootApplication
 public class PokerPlaningApplication {
 //test
